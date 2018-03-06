2e1b51ab2a423a48aa1386594cfb0fd6aaa2689b
Nickolay Polyakov <nickolay.pol@gmail.com>
2018-03-06 21:54:13 +0300

    responsive landing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <div class="app__menu" >
-      <router-link tag="md-button"  to="/">
+    <div class="app__menu">
+      <router-link tag="md-button" to="/">
         <md-icon class="app__icon">home</md-icon>
       </router-link>
-      <router-link tag="md-button" to="/about">
-      <md-icon class="app__icon">wallpaper</md-icon>
-        </router-link>
+      <!--<router-link tag="md-button" to="/about">-->
+        <!--<md-icon class="app__icon">wallpaper</md-icon>-->
+      <!--</router-link>-->
     </div>
 
     <router-view/>
@@ -14,29 +14,30 @@
 </template>
 
 <script>
-export default {
-  name: "app"
-};
+  export default {
+    name: 'app'
+  };
 </script>
 
 <style>
-@import url("http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic");
-@import url("http://fonts.googleapis.com/icon?family=Material+Icons");
+  @import url("http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic");
+  @import url("http://fonts.googleapis.com/icon?family=Material+Icons");
 
-#app {
-  font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 2em;
-}
+  #app {
+    font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 2em;
+  }
 
-.app__menu {
-  display: flex;
-  background-color: #2c3e50;
-}
-.app__icon {
-  color: #bdbdbd;
-}
+  .app__menu {
+    display: flex;
+    background-color: #2c3e50;
+  }
+
+  .app__icon {
+    color: #bdbdbd;
+  }
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="landing landing__inner">
-        <div class="row caption">
-            <div><h1 class="caption__header">TAGGER<sup>#</sup></h1></div>
-            <div class="caption__subheader">помогаем проставить теги к вашей фотографии</div>
-        </div>
-            <div class="row howto">
-            <div><h1>КАК ЭТО РАБОТАЕТ?</h1></div>
-            <div class="">
-                <div class="howto__flow">
-                    <div class="image">
-                      <img src="https://www.sunhome.ru/i/wallpapers/7/sobaka-igraet-v-pryatki.960x540.jpg">
-                      <!-- Картинка for loading -->
-                      </div>
-                    <div class="howto__upload">
-                      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"  y="0px"  width="511.626px" height="511.627px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;" xml:space="preserve">
+  <div class="landing landing__inner">
+    <div class="row caption">
+      <div><h1 class="caption__header">TAGGER<sup>#</sup></h1></div>
+      <div class="caption__subheader">проставит теги для вашей фотографии</div>
+    </div>
+    <div class="row howto">
+        <h1>КАК ЭТО РАБОТАЕТ?</h1>
+        <div class="howto__flow">
+          <div class="image-container">
+            <div class="image">
+              <img src="https://www.sunhome.ru/i/wallpapers/7/sobaka-igraet-v-pryatki.960x540.jpg">
+            </div>
+            <div class="howto__text">Загружаете фотографию</div>
+          </div>
+
+          <div class="howto__upload">
+            <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px"  y="0px"  width="511.626px" height="511.627px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;" xml:space="preserve">
                       <g>
                         <path class="arrow" d="M506.206,179.012L360.025,32.834c-3.617-3.617-7.898-5.426-12.847-5.426s-9.233,1.809-12.847,5.426
                           c-3.617,3.619-5.428,7.902-5.428,12.85v73.089h-63.953c-135.716,0-218.984,38.354-249.823,115.06C5.042,259.335,0,291.03,0,328.907
@@ -28,12 +30,19 @@
                       </g>
                       </svg>
 
-                    </div>
-                    <div class="image">
-                      <!-- Magic -->
-                      <img src="http://fraudforthought.com/wp-content/uploads/2017/02/How-AI-is-Revolutionizing-Business-Models.jpg" alt="">
-                      </div>
-                    <div class="howto__upload"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="511.626px" height="511.627px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;" xml:space="preserve">
+          </div>
+
+          <div class="image-container">
+            <div class="image">
+              <img
+                src="http://fraudforthought.com/wp-content/uploads/2017/02/How-AI-is-Revolutionizing-Business-Models.jpg"
+                alt="">
+            </div>
+            <div class="howto__text">ИИ распознает фотографию</div>
+          </div>
+
+          <div class="howto__upload">
+            <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="511.626px" height="511.627px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;" xml:space="preserve">
                       <g>
                         <path class="arrow" d="M506.206,179.012L360.025,32.834c-3.617-3.617-7.898-5.426-12.847-5.426s-9.233,1.809-12.847,5.426
                           c-3.617,3.619-5.428,7.902-5.428,12.85v73.089h-63.953c-135.716,0-218.984,38.354-249.823,115.06C5.042,259.335,0,291.03,0,328.907
@@ -46,209 +55,225 @@
                           c3.62,3.613,7.901,5.424,12.847,5.424c4.948,0,9.232-1.811,12.854-5.424l146.178-146.183c3.617-3.617,5.424-7.898,5.424-12.847
                           C511.626,186.92,509.82,182.636,506.206,179.012z"/>
                       </g>
-                      </svg></div>
-                    <div class="image__tags image ">
-                      <p>Animal,  Canine,  Dog,  Mammal,  Pet,  
-                      </p></div>
-                </div>
-                <div class="howto__texts">
-                    <div class="howto__text">Загружаете фотографию</div>    
-                    <div class="howto__text">ИИ распознает фотографию</div>    
-                    <div class="howto__text">Можете использовать теги</div>    
-                </div>
+                      </svg>
+          </div>
+
+          <div class="image-container">
+            <div class="image__tags image ">
+              <p>Animal, Canine, Dog, Mammal, Pet</p>
             </div>
-            <div class="registration-row">
-                <div class="registration-row__call-to-action">
-                    Попробуйте сейчас, это бесплатно!
-                </div>
-                <div class="registration">
-                  <div class="registration__input registration__input--email"><input  type="email" placeholder="email"></div>
-                  <div  class="registration__input registration__input--pass"><input type="password" placeholder="password"></div>
-                  <div class="registration__button"><button @click="submit()" >Регистрация</button></div>
-                  
-                </div>
-            </div>
+            <div class="howto__text">Можете использовать теги</div>
+          </div>
         </div>
-        <!-- <div class="row">
-            <div>Дполонительные данные со стоков</div>
-            <div>Обогащение ваших фотографий тегами из самых популярных фотографий с Shutterstock - возьмите теги с похожих на вашу фотографию</div>
-            <div>Process-row</div>
+      <div class="registration-row">
+        <div class="registration-row__call-to-action">
+          Попробуйте сейчас, это бесплатно!
         </div>
-        <div class="row">do not need to know english</div>
-        <div class="row">registration</div> -->
+        <div class="registration">
+          <div class="registration__input registration__input--email"><input type="email" placeholder="email"></div>
+          <div class="registration__input registration__input--pass"><input type="password" placeholder="password">
+          </div>
+          <div class="registration__button">
+            <button @click="submit()">Регистрация</button>
+          </div>
+
+        </div>
+      </div>
     </div>
+    <!-- <div class="row">
+        <div>Дполонительные данные со стоков</div>
+        <div>Обогащение ваших фотографий тегами из самых популярных фотографий с Shutterstock - возьмите теги с похожих на вашу фотографию</div>
+        <div>Process-row</div>
+    </div>
+    <div class="row">do not need to know english</div>
+    <div class="row">registration</div> -->
+  </div>
 </template>
 <script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    submit() {
-      console.log("submitted");
-      this.$router.replace({ name: "Home" });
+  export default {
+    data() {
+      return {};
+    },
+    methods: {
+      submit() {
+        console.log('submitted');
+        this.$router.replace({ name: 'Home' });
+      }
     }
-  }
-};
+  };
 </script>
 <style lang="less">
-@mainColor: rebeccapurple;
-@lightColor: aliceblue;
+  @mainColor: rebeccapurple;
+  @lightColor: aliceblue;
 
-@backgroundColor: @mainColor;
-@backgroundColorLight: @lightColor;
+  @backgroundColor: @mainColor;
+  @backgroundColorLight: @lightColor;
 
-@captionColor: @mainColor;
-// @import (css) url("http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic");
+  @captionColor: @mainColor;
+  // @import (css) url("http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic");
 
-.landing {
-  &__inner {
+  .landing {
+    &__inner {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  .row {
+    width: 100%;
     display: flex;
     flex-direction: column;
-    // background-color: red;
-  }
-}
-
-.row {
-  width: 100%;
-  //   flex-basis: 35em;
-  display: flex;
-  flex-direction: column;
-}
-
-.caption {
-  flex-basis: 35rem;
-  justify-content: center;
-  &__header {
-    font-size: 6rem;
-    color: @captionColor;
-    font-variant: small-caps;
-  }
-  &__subheader {
-    // margin-right: -20em;
-    font-size: 1.3rem;
-    color: darken(@mainColor, 20);
-  }
-}
-.howto {
-  background-color: @backgroundColor;
-  //   flex-basis: 40em;
-  padding-top: 2em;
-  color: @lightColor;
-  h1 {
-    font-weight: 400;
-    font-size: xx-large;
-  }
-  &__flow {
-    display: flex;
-    justify-content: space-around;
-    margin: 4rem 10rem;
   }
 
-  &__upload {
-    height: 10rem;
-    width: 10rem;
-    align-self: center;
+  .caption {
+    flex-basis: 35rem;
+    justify-content: center;
+    &__header {
+      font-size: 6em;
+      color: @captionColor;
+      font-variant: small-caps;
+    }
+    &__subheader {
+      position: relative;
+      left: 20vw;
+      font-size: 1.3rem;
+      color: darken(@mainColor, 20);
+      width:70vw;
+    }
   }
-  &__texts {
+
+  .howto {
+    background-color: @backgroundColor;
+    padding-top: 2em;
+    color: @lightColor;
+    h1 {
+      font-weight: 400;
+      font-size: xx-large;
+    }
+
+    &__flow {
+      display: flex;
+      justify-content: space-around;
+      align-content: space-around;
+      margin: 4rem 10rem;
+      flex-wrap: wrap;
+    }
+
+    &__upload {
+      height: 10rem;
+      width: 10rem;
+      align-self: center;
+    }
+
+    @media screen and (max-width: 1600px){
+      &__upload {
+        display: none;
+      }
+    }
+
+    &__texts {
+      display: flex;
+      justify-content: space-around;
+    }
+    &__text {
+      font-size: x-large;
+      padding-top: 2em;
+    }
+  }
+
+  .arrow {
+    fill: lighten(@backgroundColor, 30);
+
+  }
+
+  .registration-row {
     display: flex;
     justify-content: center;
-  }
-  &__text {
-    flex-basis: 30rem;
-    font-size: x-large;
-  }
-}
-
-.arrow{
-  fill:lighten(@backgroundColor,30);
-
-}
-
-.registration-row {
-  display: flex;
-  justify-content: center;
-  margin-top: 10em;
-  &__call-to-action {
-    font-size: 2em;
-    align-self: center;
-    flex-basis: 40%;
-  }
-}
-
-.registration {
-  margin: 2em;
-  border-radius: 6px;
-  background-color: @backgroundColorLight;
-  flex-basis: 20%;
-  height: 17em;
-
-  display: flex;
-  flex-direction: column;
-
-  justify-content: center;
-  align-items: flex-end;
-  padding-right: 3.7em;
-  padding-left: 3.7em;
-  padding-top: 3.7em;
-  padding-bottom: 3.7em;
-
-  &__input {
-    margin-top: 1em;
-    margin-bottom: 1em;
-    input {
-      // height: 3em;
-      border-radius: 4px;
-      padding: 0.5em 1em;
-      border: 1px solid lightgray;
-      font-size: large;
-    }
-    &--email {
-    }
-    &--pass {
+    margin-top: 5em;
+    flex-wrap: wrap;
+    &__call-to-action {
+      font-size: 2em;
+      align-self: center;
+      flex-basis: 40%;
     }
   }
-  &__button {
-    align-self: flex-end;
-    margin-top: 1em;
 
-    // padding-right: 4em;
-    button {
-      font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
-      height: 3em;
-      padding: 1em 2em;
-      background-color: #186632;
-      color: @lightColor;
-      font-size: medium;
-      border: 1px solid @mainColor;
-      border-radius: 2px;
-      &:active {
-        border: 0px;
+  .registration {
+    margin: 2em;
+    border-radius: 6px;
+    background-color: @backgroundColorLight;
+    flex-basis: 20%;
+    height: 17em;
+
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: flex-end;
+    padding: 3.7em;
+
+    &__input {
+      margin-top: 1em;
+      margin-bottom: 1em;
+
+      input {
+        border-radius: 4px;
+        padding: 0.5em 1em;
+        border: 1px solid lightgray;
+        font-size: large;
+      }
+    }
+
+    &__button {
+      align-self: flex-end;
+      margin-top: 1em;
+
+      button {
+        font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
+        height: 3em;
+        padding: 1em 2em;
+        background-color: #186632;
+        color: @lightColor;
+        font-size: medium;
+        border: 1px solid @mainColor;
+        border-radius: 2px;
+        &:active {
+          border: 0;
+        }
       }
     }
   }
-}
 
-.image {
-  height: 20rem;
-  width: 20rem;
-  // background-color: chartreuse;
-  border-radius: 1em;
-
-  &__tags {
+  .image-container{
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    font-size: large;
-    background-color: @backgroundColorLight;
-    color: black;
+  }
+  @media screen and (max-width: 1300px){
+    .image-container{
+      flex-basis: 100%;
+      align-items: center;
+    }
   }
 
-  img {
+  .image {
     height: 20rem;
-    object-fit: cover; /* Do not scale the image */
-    object-position: center;
+    width: 20rem;
     border-radius: 1em;
+
+    &__tags {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      font-size: large;
+      background-color: @backgroundColorLight;
+      color: black;
+    }
+
+    img {
+      height: 20rem;
+      object-fit: cover;
+      object-position: center;
+      border-radius: 1em;
+    }
   }
-}
 </style>
